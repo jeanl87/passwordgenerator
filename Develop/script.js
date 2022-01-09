525b797bc4200d4affca6e7b2f604278e96fd18a
Ms LJ <ljean87@gmail.com>
2022-01-09 12:19:24 -0500

    adding functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,25 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-var numbers = "0123456789"
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var lowercase = "abcdefghijklmnopqrstuvwxyz"
-var specialCharacter = "!@#$%^&*(){}[]\|/?,.~`:;'"
+var numbers = ["0123456789"];
+var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+var specialCharacter = ["!@#$%^&*(){}[]|/?,.~`:;'"];
 
-function getPasswordOptions () {
-  var pwdLength = parseInt(prompt("How long would you like your password to be"))
-  
-  if (pwdLength < 8 || pwdLength > 128) { 
-    alert("Password must be between 8 and 128 characters")
-    return
-}
-  //var numbers = confirm("Would you like to include numbers")
-  //if (numbers) {
-    //pwdLength += numbers
-  //}
+function getPasswordOptions() {
+  var pwdLength = parseInt(
+    prompt("How long would you like your password to be")
+    
+  );
+  console.log(pwdLength)
+
+  if (pwdLength < 8 || pwdLength > 128) {
+    alert("Password must be between 8 and 128 characters");
+   
+  }
+  var nums= confirm("Would you like to include numbers")
+  if (nums) {
+  pwdLength += numbers
+  console.log(nums)
+  }
 
   //var uppercase = confirm("You must include one uppercase letter")
 
@@ -23,20 +27,16 @@ function getPasswordOptions () {
 
   //var specialCharacter = confirm("You must use one special character")
 
-
-getPasswordOptions()
-
-// Write password to the #password input
-function generatePassword () {
-
+  // getPasswordOptions()
 }
+// Write password to the #password input
+function generatePassword() {}
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword); 
+generateBtn.addEventListener("click", getPasswordOptions);
